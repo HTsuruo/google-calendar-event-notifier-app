08eb0a5e9abb86e87eeab17aeabbfa28ec69a33b
tsuruo <59546923+tsuruobiz@users.noreply.github.com>
2023-05-06 00:19:36 +0900

    scheduled triggerに変更

--- triggers/daily_morning_trigger.ts
+++ triggers/daily_morning_trigger.ts
@@ -8,9 +8,24 @@ import PostTodayEventsWorkflow from "../workflows/post_today_events_workflow.ts"
  * https://api.slack.com/future/triggers
  */
 const trigger: Trigger<typeof PostTodayEventsWorkflow.definition> = {
-  type: "webhook",
+  type: "scheduled",
   name: "Post daily events at 9:00 AM",
   workflow: `#/workflows/${PostTodayEventsWorkflow.definition.callback_id}`,
+  schedule: {
+    start_time: "2023-05-06T09:00:00Z",
+    frequency: {
+      type: "weekly",
+      // TODO(tsuruoka): テスト的にSaturdayを入れているが、本番適用時には削除する
+      on_days: [
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+      ],
+    },
+  },
 };
 
 export default trigger;
