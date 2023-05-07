8addd0833255ebf8ab04783beb071023bae733f2
tsuruo <59546923+tsuruobiz@users.noreply.github.com>
2023-05-07 11:37:05 +0900

    Change timezone to JST

--- triggers/daily_morning_trigger.ts
+++ triggers/daily_morning_trigger.ts
@@ -6,17 +6,16 @@ const trigger: Trigger<typeof PostTodayEventsWorkflow.definition> = {
   name: "Post daily events at 9:00 AM",
   workflow: `#/workflows/${PostTodayEventsWorkflow.definition.callback_id}`,
   schedule: {
-    start_time: "2023-05-06T09:00:00Z",
+    start_time: "2023-05-08T09:00:00+09:00",
+    timezone: "JST",
     frequency: {
       type: "weekly",
-      // TODO(tsuruoka): テスト的にSaturdayを入れているが、本番適用時には削除する
       on_days: [
         "Monday",
         "Tuesday",
         "Wednesday",
         "Thursday",
         "Friday",
-        "Saturday",
       ],
     },
   },
